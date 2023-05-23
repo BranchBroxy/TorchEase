26864507cdbb933062eaf7ddb5d4b7f34836cede
BranchBroxy <philipp.steigerwald72@gmail.com>
2023-05-23 08:29:13 +0200

    modify setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 import codecs
 import os
 
-DESCRIPTION = 'Simplifying working with PyTorch'
-LONG_DESCRIPTION = 'TorchEase is a Python package that simplifies working with PyTorch, a popular deep learning framework. It provides a set of easy-to-use tools for training, testing, and deploying machine learning models. TorchEase includes features like early stopping, model fusing, and model evaluation, among others, which help streamline the model development process. Additionally, TorchEase is designed to be highly modular, making it easy to integrate into existing PyTorch projects.'
+description = 'Simplifying working with PyTorch'
+long_description = 'TorchEase is a Python package that simplifies working with PyTorch, a popular deep learning framework. It provides a set of easy-to-use tools for training, testing, and deploying machine learning models. TorchEase includes features like early stopping, model fusing, and model evaluation, among others, which help streamline the model development process. Additionally, TorchEase is designed to be highly modular, making it easy to integrate into existing PyTorch projects.'
 
 with open(os.path.join(os.path.dirname(__file__), "TorchEase", "VERSION")) as version_file:
     version = version_file.read().strip()
@@ -15,9 +15,9 @@ setup(
     version=version,
     author="Philipp Steigerwald",
     author_email="info@b-stream.info",
-    description=DESCRIPTION,
+    description=description,
     long_description_content_type="text/markdown",
-    long_description=LONG_DESCRIPTION,
+    long_description=long_description,
     packages=find_packages(),
     install_requires=["scikit-learn", "matplotlib", "pandas", "torch", "numpy", "scipy"],
     keywords=['python', 'torch', 'pytorch', 'early stop', 'trainer', 'tensor'],
